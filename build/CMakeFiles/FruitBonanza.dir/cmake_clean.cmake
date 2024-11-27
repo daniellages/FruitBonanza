a8118d0a1c7ffccb94904fd090b3cd69c119e1d4
Daniel Lages <daniel.lages.2000@gmail.com>
2024-11-27 09:42:11 +0000

    Swapped from MakeFile to cmake

---
+++ build/CMakeFiles/FruitBonanza.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FruitBonanza.dir/helper/probabilities.cpp.obj"
+  "CMakeFiles/FruitBonanza.dir/helper/probabilities.cpp.obj.d"
+  "CMakeFiles/FruitBonanza.dir/src/GridItem.cpp.obj"
+  "CMakeFiles/FruitBonanza.dir/src/GridItem.cpp.obj.d"
+  "CMakeFiles/FruitBonanza.dir/src/main.cpp.obj"
+  "CMakeFiles/FruitBonanza.dir/src/main.cpp.obj.d"
+  "bin/main.exe"
+  "bin/main.exe.manifest"
+  "bin/main.pdb"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FruitBonanza.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
